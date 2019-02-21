136f1775a20aaddaf4713c90230c2999cd9bf3af
Boopathi Krishnan <boopathi.career@live.com>
2019-02-21 15:37:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,26 +10,3 @@ app.get('/', (req,res) => {
   app.listen(port,() => {
     console.log(`App Server Listening at ${port}`);
   });
-
-var myEvent= require('events');
-
-var targetEvent = new myEvent.EventEmitter;
-
-targetEvent.on("listenTo",(value)=>{
-    console.log("event triggered with on  "+ value);
-});
-targetEvent.addListener("listenTo1",(value)=>{
-    console.log("event triggered with addListener "+ value);
-})
-targetEvent.once("listenTo2",(value)=>{
-    console.log("event triggered once "+ value);
-})
-targetEvent.once("error",(value)=>{
-    console.log("error!  "+ value);
-})
-
-  targetEvent.emit('listenTo',"Boopathi");
-  targetEvent.emit('listenTo1',"Boopathi1");
-  targetEvent.emit('listenTo2',"Boopathi2");
-  targetEvent.emit('listenTo2',"Boopathi2");
-  targetEvent.emit('error',new Error('whoops!'));
