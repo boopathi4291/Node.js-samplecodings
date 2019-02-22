3c65fbc65d99fd0060c4494b79c1a2bac0ff0aaf
Boopathi Krishnan <boopathi.career@live.com>
2019-02-22 11:32:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port =  8080;
 
 app.get('/', (req,res) => {
-    return res.sendFile('sample.html',, { root: __dirname });
+    return res.sendFile('sample.html', { root: __dirname });
   });
 
   app.listen(port,() => {
