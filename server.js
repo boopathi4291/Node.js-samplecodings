bf06b9eba3445d7f1d964192dc4cadf2ff67b499
Boopathi Krishnan <boopathi.career@live.com>
2019-02-22 13:17:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const port =  8080;
 
 app.get('/', (req,res) => {
-    return res.sendFile('sample.html', { root: __dirname });
+    return res.sendFile("/templates/sample.html", { root: __dirname });
+  });
+
+app.get('/getExample/:name/:office', (req,res) => {
+    let name=req.params.name;
+    let office = req.params.office;
+    return res.send(name+" is working in "+office+ ".");
+  });
+
+  app.post('/login',(req,res)=>{
+console.log(req);
+
   });
 
   app.listen(port,() => {
